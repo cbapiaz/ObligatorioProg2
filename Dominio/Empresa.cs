49772f26a2fe8c06408ae61502b1337d938fdb46
Sebastian <cbapiazza@gmail.com>
2021-04-27 16:03:55 -0300

    add main methods to main sistem class

---
+++ Dominio/Empresa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class Empresa
+    {
+        public List<Paquete> Paquetes { get;  }
+        public List<Canal> Canales { get; }
+
+
+        public Empresa()
+        {
+            Paquetes = new List<Paquete>();
+            Canales = new List<Canal>();
+        }
+        //TODO
+        //prepopulacion como vimos en clase - TODO
+
+        public void PrecargaPaquetes()
+        {
+
+        }
+
+        public void PrecargaCanales()
+        {
+
+        }
+
+
+        public bool AgregarCanal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AgregarPaquete()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public List<Paquete> ListarPaquetes()
+        {
+
+            return Paquetes;
+        }
+
+        public List<Canal> ListarCanales()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public List<Paquete> ListarPaquetesConMasCanales()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public List<Paquete> ListarPaquetesConMayorPrecio(decimal precio)
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
