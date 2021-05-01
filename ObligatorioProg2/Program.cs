d0226784d0328586184207048ea7a1ce5b74fe66
Sebastian <cbapiazza@gmail.com>
2021-05-01 19:38:38 -0300

    listar paquetes listar canales program

--- ObligatorioProg2/Program.cs
+++ ObligatorioProg2/Program.cs
@@ -5,13 +5,29 @@ namespace ObligatorioProg2
 {
     class Program
     {
-        readonly Empresa empresa = new Empresa();
+        private static Empresa empresa = new Empresa();
         static void Main(string[] args)
         {
 
             Canal c = new Canal("pepe",Resolucion.BAJA,false,7878);
             Console.WriteLine(c);
+
+            Console.WriteLine("Canales");
+            foreach (var v in empresa.ListarCanales())
+            {
+                Console.WriteLine(v.ToString());
+            }
+
+
+            Console.WriteLine("Paquetes");
+
+            foreach (var v in empresa.ListarPaquetes())
+            {
+                Console.WriteLine(v.ToString());
+            }
+
             Console.ReadLine();
+
         }
     }
 }
