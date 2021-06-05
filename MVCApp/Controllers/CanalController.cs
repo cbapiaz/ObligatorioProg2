df96e49b4176bc830397dc3552b27fce8cc641f1
Sebastian <cbapiazza@gmail.com>
2021-06-05 15:57:32 -0300

    commit new files

---
+++ MVCApp/Controllers/CanalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class CanalController : Controller
+    {
+        // GET: Canal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
