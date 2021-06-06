aea4355ff2cc6062f802aaad4e203df97d58dcc5
DESKTOP-S6NE8OF\GameGear <mathiassanchez1989@hotmail.com>
2021-06-06 12:01:15 -0300

    Listas  de canal y paquete

--- MVCApp/Controllers/CanalController.cs
+++ MVCApp/Controllers/CanalController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Dominio;
 namespace MVCApp.Controllers
 {
     public class CanalController : Controller
     {
+        Empresa unE =Empresa.Instancia;
         // GET: Canal
         public ActionResult Index()
         {
+            ViewBag.canales = unE.ListarCanales();
+
             return View();
         }
+
     }
 }
--- MVCApp/Controllers/PaqueteController.cs
+++ MVCApp/Controllers/PaqueteController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Dominio;
 namespace MVCApp.Controllers
 {
     public class PaqueteController : Controller
     {
+        Empresa unP = Empresa.Instancia;
         // GET: Paquete
         public ActionResult Index()
         {
+            ViewBag.paquetes = unP.ListarPaquetes();
             return View();
         }
     }
